81eb0cb044eb369a9dd5328d9cf43b1b70f18619
Miguel Angel <miguelksangel@gmail.com>
2021-03-05 16:56:48 -0600

    Comienzo de servidor nodejs y python

---
+++ Servidores/Server js/users.js
@@ -0,0 +1,15 @@
+let aws_keys = {
+    s3: {
+        region: 'us-east-2',
+        accessKeyId: "AKIAYNCEHQNLPYYPEDNP",
+        secretAccessKey: "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",
+        //apiVersion: '2006-03-01',
+    },
+    dynamodb: {
+        //apiVersion: '2012-08-10',
+        region: 'us-east-2',
+        accessKeyId: "AKIAYNCEHQNLMEBWPNGK",
+        secretAccessKey: "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"
+    }
+}
+module.exports = aws_keys
