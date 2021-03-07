8fac0cdac2a8d1f6e9e2a64743fec1ba3734041a
Miguel Angel <miguelksangel@gmail.com>
2021-03-07 17:23:54 -0600

    Servidores terminados

--- Servidores/Server py/index.py
+++ Servidores/Server py/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, json
 import boto3, base64, os, MySQLdb, uuid
 from botocore.config import Config
+from flask_cors import CORS
 
 #import sys
 #import boto3
@@ -22,44 +23,52 @@ perfil = "Fotos_Perfil"
 publicas = "Fotos_Publicadas"
 
 app = Flask(__name__)
+CORS(app)
 
 '''---------------------CONSULTAS A MYSQL--------------------'''
 @app.route('/crearUsuario', methods=['POST']) 
 def crearUsuario():
     print("CREAR USUARIO----------------->")
+    
     try:
         req = request.get_json()
         user = req['user']
         name = req['name']
         pas = req['pass']
         consulta = f'insert into Usuario(codigo_usuario, nombre, psw) values ("{user}","{name}","{pas}");'
-        
-        cur.execute(consulta)
-        conn.commit()
-        return 'si'
-
+    
+        a = cur.execute(consulta)
+        if(a):
+            conn.commit()
+            return '{"res":"si"}'
+        else:
+            return 'null'
     except Exception as e:
         return 'null'
 
 @app.route('/crearAlbum', methods=['POST']) 
 def crearAlbum():
     print("CREAR ALBUM----------------->")
+    
     try:
         req = request.get_json()
         user = req['user']
         album = req['album']
         consulta = f'insert into Album(nombre, usuario_codigo) values ("{album}","{user}");'
-        
-        cur.execute(consulta)
-        conn.commit()
-        return 'si'
 
+        a = cur.execute(consulta)
+        if(a):
+            conn.commit()
+            return '{"res":"si"}'
+        else:
+            return 'null'
     except Exception as e:
         return 'null'
 
 @app.route('/crearFoto', methods=['POST']) 
 def crearFoto():
     print("CREAR FOTO----------------->")
+    
     try:
         req = request.get_json()
         user = req['user']
@@ -87,10 +96,13 @@ def crearFoto():
         response = client.Bucket('practica1-g30-imagenes').put_object(Key=img, Body=image_64_decode, ContentType="image", ACL='public-read')
 
         consulta = f'insert into Foto(nombre, url_fotop, album_codigo) values ("{nombreFoto}","{url}",(select codigo_album from Album where nombre = "{album}" and usuario_codigo = "{user}" group by codigo_album));'
-        
-        cur.execute(consulta)
-        conn.commit()
-        return '{"res": "' + url +'"}'
+
+        a = cur.execute(consulta)
+        if(a):
+            conn.commit()
+            return '{"res": "' + url +'"}'
+        else:
+            return 'null'
 
     except Exception as e:
         return 'null'
@@ -98,6 +110,7 @@ def crearFoto():
 @app.route('/modificarUsuario', methods=['POST']) 
 def modificarUsuario():
     print("MODIFICAR USUARIO----------------->")
+    
     try:
         req = request.get_json()
         user = req['user']
@@ -106,35 +119,129 @@ def modificarUsuario():
         url = req['url']
 
         consulta = f'update Usuario set codigo_usuario="{userNew}", nombre="{nombre}", url_fotop="{url}" where codigo_usuario="{user}";'
-        
+
         a = cur.execute(consulta)
-        print(a)
-        conn.commit()
-        return '{"res":"si"}'
+        if(a):
+            conn.commit()
+            return '{"res":"si"}'
+        else:
+            return 'null'
+    except Exception as e:
+        return 'null'
+
+@app.route('/buscarUsuario', methods=['POST']) 
+def buscarUsuario():
+    print("BUSCAR USUARIO----------------->")
+    
+    
+    try:
+        req = request.get_json()
+        user = req['user']
+
+        consulta = f'select * from Usuario where codigo_usuario = "{user}";'
+    
+        a = cur.execute(consulta)
+        if(a):
+            row_headers=[x[0] for x in cur.description]
+
+            json_data=[]
+            for result in cur:
+                json_data.append(dict(zip(row_headers,result)))
+            return json.dumps(json_data)
+        else:
+            return 'null'
 
     except Exception as e:
         return 'null'
 
+@app.route('/buscarFotos', methods=['POST']) 
+def buscarFotos():
+    print("BUSCAR FOTOS----------------->")
+    
+    try:
+        req = request.get_json()
+        user = req['user']
 
+        consulta = f'select F.nombre as nombreF, F.url_fotop, A.nombre as nombreA from Foto F, Album A where A.usuario_codigo = "{user}" and F.album_codigo = A.codigo_album;'
+        
+        a = cur.execute(consulta)
+
+        if(a):
+            row_headers=[x[0] for x in cur.description]
+
+            json_data=[]
+            for result in cur:
+                json_data.append(dict(zip(row_headers,result)))
+            return json.dumps(json_data)
+        else:
+            return 'null'
+
+    except Exception as e:
+        return 'null'
+
+@app.route('/buscarAlbum', methods=['POST']) 
+def buscarAlbum():
+    print("BUSCAR ALBUM----------------->")
+    
+    try:
+        req = request.get_json()
+        user = req['user']
+
+        consulta = f'select nombre from Album where usuario_codigo = "{user}" and nombre != "Fotos_Perfil";'
+    
+        a = cur.execute(consulta)
+        if(a):
+            row_headers=[x[0] for x in cur.description]
+
+            json_data=[]
+            for result in cur:
+                    json_data.append(dict(zip(row_headers,result)))
+            return json.dumps(json_data)
+        else:
+            return 'null'
+
+    except Exception as e:
+        return 'null'
+
+@app.route('/eliminarAlbum', methods=['POST']) 
+def eliminarAlbum():
+    print("ELIMINAR ALBUM----------------->")
+    
+    try:
+        req = request.get_json()
+        user = req['user']
+        album = req['album']
+
+        consulta = f'delete from Album where nombre="{album}" and usuario_codigo="{user}";'
+    
+        a = cur.execute(consulta)
+        if(a):
+            conn.commit()
+            return '{"res":"si"}'
+        else:
+            return 'null'
+
+    except Exception as e:
+        return 'null'
 
 '''--------------------------PRUEBAS------------------------'''
 @app.route('/', methods=['POST']) 
 def index():
     try:
         response = request.get_json()
-        #img_b = response['foto']
-    
-        cur.execute('select * from Album;')
-        row_headers=[x[0] for x in cur.description]
-        #query_results = cur.fetchall()
-
-        json_data=[]
-        for result in cur:
-                json_data.append(dict(zip(row_headers,result)))
-        return json.dumps(json_data)
+        a = cur.execute('select * from Usuario;')
+        
+        if(a):
+            row_headers=[x[0] for x in cur.description]
+            json_data=[]
+            for result in cur:
+                    json_data.append(dict(zip(row_headers,result)))
+            return json.dumps(json_data)
+        else:
+            return 'null'
 
     except Exception as e:
-        return null
+        return 'null'
 
 @app.route('/up', methods=['POST']) 
 def upload():
@@ -167,4 +274,4 @@ def upload():
     return 'exito'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)#host='0.0.0.0',
